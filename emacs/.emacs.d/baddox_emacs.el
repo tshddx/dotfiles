e9096209795c9111e0903fb0518ed22962032761
Thomas Shaddox <wediddoit@gmail.com>
2011-04-12 10:56:05 -0500

    Trying another method to keep emacs' junk files out of my directories.

--- emacs/.emacs.d/baddox_emacs.el
+++ emacs/.emacs.d/baddox_emacs.el
@@ -25,8 +25,22 @@
 ;; from http://emacsblog.org/2008/05/19/giving-ido-mode-a-second-chance/
 (setq ido-enable-flex-matching t)
 
+;; ;; Make sure all backup files only live in one place
+;; (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+
 ;; Make sure all backup files only live in one place
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+;; http://amitp.blogspot.com/2007/03/emacs-move-autosave-and-backup-files.html
+(defvar user-temporary-file-directory
+  (concat temporary-file-directory user-login-name "/"))
+(make-directory user-temporary-file-directory t)
+(setq backup-by-copying t)
+(setq backup-directory-alist
+      `(("." . ,user-temporary-file-directory)
+        (,tramp-file-name-regexp nil)))
+(setq auto-save-list-file-prefix
+      (concat user-temporary-file-directory ".auto-saves-"))
+(setq auto-save-file-name-transforms
+      `((".*" ,user-temporary-file-directory t)))
 
 ;; -----------------------------
 ;; KEY BINDINGS and HELPER FUNCTIONS
